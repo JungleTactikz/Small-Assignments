b2c168a0839dca483b71684e4aaabd999431d2f7
Thorsteinn Thorvaldsson <thorsteinnt09@gmail.com>
2011-07-20 23:20:21 +0000

    Removed special characters

--- BestCompressionEver.cpp
+++ BestCompressionEver.cpp
@@ -44,29 +44,7 @@ int main()
     // The test output fileð
     //write("out.txt");
 
-    // Formúlan er (uint64_t)pow(2.0, ((float)b+1)) - 3, en fer í einhverja steik með risatölur, þótt ég sé á 64bita örgj...
-    // þannig ég bjó til þessa hér fyrir neðan, einnig virðist þessi aðferð vera örlítið hraðvirkari en að nota formúluna(miðað við 1000000 keyrslur í röð)
-
-    // Setjum s = 1 í byrjun, loopum svo jafn oft og bitarnir eru (gefnir í inntakinu) margir, bitwise OR svo s við sjálft sig bit-shiftað um 1
-    // ef s er minna en n í lokin þá gengur það sem við fengum í inntakinu ekki upp.
-
-    /*
-        Dæmi fyrir 13 3
-        0001
-        0010
-         ---
-        0011
-        0110
-         ---
-        0111
-        1110
-        ----
-        1111 = 15
-
-        Getum svo dregið 2 frá til að fá nákvæma lausn, 15 - 2 = 13
-        Að sleppa því að draga 2 frá hefur þó engin áhrif á úttakið
-    */
-
+    
     uint64_t n,b,s=1,i=1;
     cin>>n>>b;
     for(;i++<=b;s|=(s<<1));
